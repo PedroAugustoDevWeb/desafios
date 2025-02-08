fc8220996bf201e82453ee380daa9804cd14a9c0
Pedro Augusto <pedroaugustodev4@gmail.com>
2025-02-08 15:37:21 -0300

    process models

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/models/User.java
@@ -0,0 +1,42 @@
+package com.picpaysimplificado.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fistName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(unique = true)
+    private String cpf;
+
+
+
+}
