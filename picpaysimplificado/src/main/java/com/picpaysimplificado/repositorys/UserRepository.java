b4ade0459ca054bfb62b449a3e2dc3ef8f836df3
Pedro Augusto <pedroaugustodev4@gmail.com>
2025-02-08 21:08:41 -0300

    create repositorys

--- picpaysimplificado/src/main/java/com/picpaysimplificado/repositorys/UserRepository.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositorys/UserRepository.java
@@ -1,10 +1,15 @@
 package com.picpaysimplificado.repositorys;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.picpaysimplificado.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findUserByCpf(String cpf);
+
+    Optional<User> findUserById(Long id);
     
 } 
